35d699eb923ef0b89fa7c1562930ee4eb62215b5
Bulent Cesur <46667756+mesleki2017@users.noreply.github.com>
2024-09-23 15:10:49 +0300

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,3 +53,17 @@ const scrollReveal = function () {
 
 window.addEventListener("scroll", scrollReveal);
 window.addEventListener("load", scrollReveal);
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
